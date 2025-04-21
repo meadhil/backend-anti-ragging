4b7603837b54e3eabcf6e54012e08a07a2ab7898
meadhil <mohammed__adhil@outlook.com>
2025-04-21 08:44:27 +0530

    final

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },          // 🆕 Added name
+  department: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'user'], default: 'user' },
